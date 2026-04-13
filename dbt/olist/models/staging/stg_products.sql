f84de7e25ca089ea1b61b203a3ae18f9941c4d65
Saurabh Jain <saurabhjainjs@gmail.com>
2026-04-13 22:02:33 +0000

    add dbt intermediate and mart models with tests

--- dbt/olist/models/staging/stg_products.sql
+++ dbt/olist/models/staging/stg_products.sql
@@ -5,14 +5,14 @@ with source as (
 renamed as (
     select
         product_id,
-        product_category_name           as category_name,
-        product_name_lenght             as product_name_length,
-        product_description_lenght      as product_description_length,
-        product_photos_qty              as photos_qty,
-        product_weight_g                as weight_g,
-        product_length_cm               as length_cm,
-        product_height_cm               as height_cm,
-        product_width_cm                as width_cm
+        coalesce(product_category_name, 'unknown')           as category_name,
+        product_name_lenght                                  as product_name_length,
+        product_description_lenght                           as product_description_length,
+        product_photos_qty                                   as photos_qty,
+        product_weight_g                                     as weight_g,
+        product_length_cm                                    as length_cm,
+        product_height_cm                                    as height_cm,
+        product_width_cm                                     as width_cm
     from source
     where product_id is not null
 )
